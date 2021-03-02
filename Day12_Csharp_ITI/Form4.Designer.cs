a96f279e3b3e1cafac60a77592f5bad13eedb5ab
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2021-03-03 00:25:52 +0200

    Mickey Mouse

--- Day12_Csharp_ITI/Form4.Designer.cs
+++ Day12_Csharp_ITI/Form4.Designer.cs
@@ -29,47 +29,56 @@ namespace Day12_Csharp_ITI
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(128, 77);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(50, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(128, 132);
+            this.button1.Location = new System.Drawing.Point(120, 132);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(41, 29);
+            this.button1.Size = new System.Drawing.Size(56, 29);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "X";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(324, 132);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(56, 29);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "+";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button2_MouseDown);
             // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(773, 450);
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.ClientSize = new System.Drawing.Size(482, 402);
+            this.ControlBox = false;
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form4";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
             this.Text = "Form4";
+            this.TransparencyKey = System.Drawing.Color.Maroon;
             this.Load += new System.EventHandler(this.Form4_Load);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form4_Paint);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- Day12_Csharp_ITI/Form4.cs
+++ Day12_Csharp_ITI/Form4.cs
@@ -18,7 +18,7 @@ namespace Day12_Csharp_ITI
 
         private void Form4_Load(object sender, EventArgs e)
         {
-
+        
         }
 
         private void Form4_Paint(object sender, PaintEventArgs e)
@@ -48,5 +48,36 @@ namespace Day12_Csharp_ITI
 
             // Gr.DrawPath(new Pen(Color.Black),G);
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+       
+    
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void button2_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        
     }
 }
